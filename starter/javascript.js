351a01869031716858214025c4b6441e54300871
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-27 19:13:27 +0000

    added pseudocode

---
+++ starter/javascript.js
@@ -0,0 +1,8 @@
+//--------pseudocode--------//
+//step 1 : add moment.js into the 'jumbotron' to display the date and time.
+
+//step 2 : create my rows in html that will contain a time stamp, a textarea, am a save button.
+
+//step 3 : use jquery to change the colours of each timeblock based on past, present, and future when the timeblock is viewed.
+
+//step 4 : Allow a user to enter an event when they click a timeblock and allow them to save the value to local storage
