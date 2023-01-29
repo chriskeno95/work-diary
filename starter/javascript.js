1040367c7314e42c8901b22f03263e95836ce07a
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-29 20:50:27 +0000

    added live time and date

--- starter/javascript.js
+++ starter/javascript.js
@@ -6,3 +6,12 @@
 //step 3 : use jquery to change the colours of each timeblock based on past, present, and future when the timeblock is viewed.
 
 //step 4 : Allow a user to enter an event when they click a timeblock and allow them to save the value to local storage
+
+let currentDay = $("#currentDay");
+// code to add date and time to currentDayID
+var today = moment();
+
+//code to live update the time
+window.setInterval(function () {
+    currentDay.html(moment().format("dddd, D MMMM YYYY, h:mm:ss a"))
+}, 1000);
