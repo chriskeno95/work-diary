a7bd4ac9b49e2d398b110a0886823a5a01b96da0
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-29 23:11:10 +0000

    added some styling and rest of the rows. also created the function for displaying notification

--- starter/javascript.js
+++ starter/javascript.js
@@ -7,6 +7,11 @@
 
 //step 4 : Allow a user to enter an event when they click a timeblock and allow them to save the value to local storage
 
+//creating an array of timestamps that i will try and create rows for - this will contain the time, a type box and a save button
+let notification = $('#notification');
+let saveEvent = $(".saveEvent");
+let diary = $(".container");
+let input = $(".description");
 let currentDay = $("#currentDay");
 // code to add date and time to currentDayID
 var today = moment();
@@ -15,3 +20,12 @@ var today = moment();
 window.setInterval(function () {
     currentDay.html(moment().format("dddd, D MMMM YYYY, h:mm:ss a"))
 }, 1000);
+
+
+//function to display notification
+saveEvent.on("click", function(){
+    notification.removeClass("hide");
+    setTimeout(function(){
+    notification.addClass("hide");   
+},2000)
+})
