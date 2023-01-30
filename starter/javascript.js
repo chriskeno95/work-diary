df260bb1c5c84432dbb2a9dbe0aeddd856636768
chris kennedy <chris-kennedy09@hotmail.com>
2023-01-30 19:15:35 +0000

    finished the local storage call back to display the value of the saved key

--- starter/javascript.js
+++ starter/javascript.js
@@ -10,12 +10,7 @@ $(document).ready(function(){
 //creating an array of timestamps that i will try and create rows for - this will contain the time, a type box and a save button
 let notification = $('#notification');
 let saveEvent = $(".saveEvent");
-let diary = $(".container");
-let input = $(".description");
 let currentDate = $("#currentTime");
-// code to add date and time to currentDayID
-var today = moment();
-
 
 //code to live update the time
 window.setInterval(function () {
@@ -32,24 +27,26 @@ saveEvent.on("click", function(){
 
 //function to save description input and time to localStorage
 let time = $(this).parent().attr("id");
-let event = $(this).siblings(".description").val();
+let event = $(this).siblings(".description").val().trim();
 
 localStorage.setItem(time,event);
-
 
 })
 
 //function to check current time and adjust row bg colour to determine past/present/future
 function hourStatus(){
     var currentHour = moment().hours();
-
+//first i set the current hour and create a forloop to run through the sections with the class 'time-block'
     for(let i = 0; i < $(".time-block").length; i++){
+//secondly i grab the id from the classes and store the value as the hourID for each section
     let hourID = parseInt($(".time-block")[i].getAttribute('id'));
+//thirdly i create if statements to compare the hourID with the current hour and execute code that will add or remove classes.
     if (hourID < currentHour){
         $(".time-block")[i].classList.add("past");
     } else if(hourID == currentHour){
         $(".time-block")[i].classList.remove("past")
         $(".time-block")[i].classList.add("present");
+    // initially i tried $(".time-block")[i].addClass("present"); but this did not work so i have used $(".time-block")[i].classList.remove("past") - i got this from the speed run
     } else {
         $(".time-block")[i].classList.remove("past")
         $(".time-block")[i].classList.remove("present")
@@ -58,4 +55,16 @@ function hourStatus(){
 }
 }
 hourStatus();
+
+//below i am selecting the description from every ID avaiable and setting the value for the textarea to be the value from the local storage with the key matching to the ID
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
 })
